a355dfda82b1cf219122d1ba787253aafb40f082
mhope123 <119423048+mhope123@users.noreply.github.com>
2022-11-29 13:28:31 -0300

    Update conexao.php

--- adm/config/conexao.php
+++ adm/config/conexao.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 define('HOST', 'ec2-3-216-167-65.compute-1.amazonaws.com');
+define('PORT', '5432');
 define('DB', 'db9hcdeb4icket');
 define('USUARIO', 'kweuppelflmvip');
 define('SENHA', 'd36b4d5a4eb681723f4ea8bbbd4530aadeeb9aa2c52a3d725dcfde8ad256b740');
@@ -8,7 +9,7 @@ define('SENHA', 'd36b4d5a4eb681723f4ea8bbbd4530aadeeb9aa2c52a3d725dcfde8ad256b74
 global $conexao;
 
 try{
-    $conexao = new PDO('mysql:host=' . HOST . ';dbname=' . DB . ';', USUARIO, SENHA);
+    $conexao = new PDO('mysql:host=' . HOST . '; port=' . PORT . ' ;dbname=' . DB . ';', USUARIO, SENHA);
     //echo "Conexão com banco de dados ok!";
 }catch(PDOException $erro){
     echo "Erro na conexão com banco de dados!",  $erro->getMessage();
